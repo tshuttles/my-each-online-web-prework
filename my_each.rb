9c46ae34e24980692dd1a7b5cb4c12737e95aae6
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-13 01:00:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,17 +1,8 @@
 def my_each(array)
-  x = [] 
+  x = []
   while array < array.length 
-    yield array[x]
-    x = x + 1 
+    yield(array[x])
+    x += 1 
   end 
-  x 
-end
-
-def my_each(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-  array
+  array 
 end
